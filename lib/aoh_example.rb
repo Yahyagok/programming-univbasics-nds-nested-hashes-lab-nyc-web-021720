71a7fbeac34fca1559a39165587e9629526451f2
Yahya  Gok <ygok17@hotmail.com>
2020-01-27 21:28:11 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -39,7 +39,7 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
-  aoh[row][:name]
+  aoh[row][:key]
 end
 def aoh_update(aoh, row, key, new_value)
   # Update the AoH data at row and key to have the value of new_value
